4954e55b1cc1c795aa1c83321f093df270d080bc
ZaynZachary <dsouzayn@gmail.com>
2026-08-20 12:04:58 +1000

    created requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
+import dotenv
 
 import discord
 from discord.ext import commands
+
+dotenv.load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="+", intents=intents)
-
 
 @bot.event
 async def on_ready():
