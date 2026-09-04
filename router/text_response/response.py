0b3bff42f1e7c341e6d25f1324f5c01ddf639fbd
Sangeet Saha <sangeetsaha1116@gmail.com>
2026-09-03 23:34:17 -0400

    test logging

--- router/text_response/response.py
+++ router/text_response/response.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 @commands.command()
 async def ping(ctx):
 	"""Check whether the bot is responding."""
-	logger.info(f"Sending response for ping with pong user: {ctx}")
+	logger.info(f"Sending response for ping with pong user: {ctx.author.id}")
 	await ctx.send("Pong2!")
 
 @commands.command()
