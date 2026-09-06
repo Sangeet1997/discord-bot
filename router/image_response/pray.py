0634d58fb0b138ce405d9747c79840612c3bae7d
Sangeet Saha <sangeetsaha1116@gmail.com>
2026-09-06 00:08:31 -0400

    added new command for balance, pray

--- router/image_response/pray.py
+++ router/image_response/pray.py
@@ -6,27 +6,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 @commands.command()
-async def ping(ctx):
+async def pray(ctx):
 	"""Check whether the bot is responding."""
 	logger.info(f"Sending response for ping with pong user: {ctx.author.id}")
 	await ctx.send("Pong2!")
 
-@commands.command()
-async def ding(ctx):
-	"""Check whether the bot is responding."""
-	await ctx.send("Dong!")
 
-@commands.command()
-async def ching(ctx):
-	"""Check whether the bot is responding."""
-	await ctx.send("Chong!")
-
-@commands.command()
-async def ching(ctx):
-	"""Check whether the bot is responding."""
-	await ctx.send("Chong!")
 
 async def setup(bot):
-    bot.add_command(ping)
-    bot.add_command(ding)
-    bot.add_command(ching)
+    bot.add_command(pray)
