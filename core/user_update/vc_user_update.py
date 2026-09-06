aa3fce39d00901f6f6046eda3c87f8cca2cd47f3
Sangeet Saha <sangeetsaha1116@gmail.com>
2026-09-06 00:12:11 -0400

    decreased points for mute

--- core/user_update/vc_user_update.py
+++ core/user_update/vc_user_update.py
@@ -42,7 +42,7 @@ class VcUserUpdateCog(commands.Cog):
             xp = settings.INTERVAL_XP_AMOUNT
 
             if voice:
-                if voice.self_deaf or voice.deaf:
+                if voice.self_deaf or voice.deaf or voice.self_mute or voice.mute:
                     points = settings.DEAFENED_INTERVAL_POINT_AMOUNT
                 elif voice.self_stream or voice.self_video:
                     points += half_points
